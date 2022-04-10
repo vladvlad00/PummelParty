cd2902a57ced97858d8bbde8766afbe5fc6562f8
David-Andrei Viziteu <31017005+davidviziteu@users.noreply.github.com>
2022-04-10 21:14:02 +0300

    added arrows between points

--- Assets/Scripts/GameGuard.cs
+++ Assets/Scripts/GameGuard.cs
@@ -13,7 +13,7 @@ public class GameGuard : MonoBehaviour
     public GameObject board;
 
     public List<Player> players;
-
+    public GameObject pathPrefab;
     [NonSerialized]
     public List<Spot> boardSpots;
 
@@ -42,7 +42,47 @@ public class GameGuard : MonoBehaviour
         for(int i = 0; i < boardSpots.Count; ++i)
         {
             boardSpots[i].index = i;
+            
         }
+
+        //lambda function that takes 2 positions and returns the distance between them
+        Func<Vector3, Vector3, float> distance = (a, b) =>
+        {
+            return Mathf.Sqrt(Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.z - b.z, 2));
+        };
+
+
+        //lambda function that takes 2 positions and returns the angle between them
+        Func<Vector3, Vector3, float> angleBetween = (a, b) =>
+        {
+            Vector3 dir = b - a;
+            float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+            return angle;
+        };
+
+        for (int i = 0; i < boardSpots.Count; ++i)
+        {
+            //aici pun sagetile     
+            for (int j = 0; j < boardSpots[i].next.Count; j++)
+            {
+                //pui sageata aici
+                GameObject obj = Instantiate(pathPrefab,
+                    new Vector3(
+                        (boardSpots[i].transform.position.x + boardSpots[i].next[j].transform.position.x) / 2,
+                        0.2f,
+                        (boardSpots[i].transform.position.z + boardSpots[i].next[j].transform.position.z) / 2
+                    ),
+                    Quaternion.Euler(-90, 180 + angleBetween(boardSpots[i].next[j].transform.position, boardSpots[i].transform.position), 0)
+                );
+                float spriteHeight = obj.GetComponent<SpriteRenderer>().bounds.size.y * obj.GetComponent<Transform>().localScale.y;
+                obj.GetComponent<Transform>().localScale = new Vector3(
+                    obj.GetComponent<Transform>().localScale.x,
+                    distance(boardSpots[i].transform.position, boardSpots[i].next[j].transform.position) / (spriteHeight) / 100,
+                    obj.GetComponent<Transform>().localScale.z
+                );
+            }
+        }
+
 
         GameMaster.INSTANCE.OnReturnToGame();
 
