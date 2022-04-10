4fa2b5309a6b0d68ac3a293714ae89756a5b9ac5
UnexomWid <uw@exom.dev>
2022-04-10 16:47:26 +0300

    add: hp, crowns and item system

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿public class Item
+{
+    public string name;
+    public string description;
+
+    public Delegates.UseItemDelegate onUse;
+
+    public Item(string name, string description, Delegates.UseItemDelegate onUse)
+    {
+        this.name = name;
+        this.description = description;
+        this.onUse = onUse;
+    }
+
+    public void Use(PlayerData player)
+    {
+        onUse(player);
+    }
+}
--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,17 +1,51 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
 
 [Serializable]
 public class PlayerData
 {
+    public const int MAX_HP = 20;
+
     public int id;
     public string name;
+
     [NonSerialized]
     public int spot = -1;
+
+    [NonSerialized]
+    public int crowns = 0;
+
+    [NonSerialized]
+    public int hp = MAX_HP;
+
+    [NonSerialized]
+    public List<ItemStack> stacks = new List<ItemStack>();
 
     public Spot GetSpot()
     {
         Debug.Assert(spot != -1, "GetSpot was called, but the spot wasn't assigned to");
         return GameGuard.INSTANCE.boardSpots[spot];
+    }
+
+    public void TakeDamage(int amount)
+    {
+        hp -= amount;
+
+        if(hp <= 0)
+        {
+            TeleportToSpot(GameMaster.INSTANCE.guard.graveyardSpot);
+
+            hp = MAX_HP;
+        }
+    }
+
+    void TeleportToSpot(Spot spot)
+    {
+        this.spot = spot.index;
+        Transform transform = GameMaster.INSTANCE.guard.players[id].transform;
+
+        // Preserve player Y
+        transform.position = new Vector3(spot.transform.position.x, transform.position.y, spot.transform.position.z);
     }
 }
