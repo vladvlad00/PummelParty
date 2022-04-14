cee243c0527f1d381d4cda69f399edc7c7ba4984
UnexomWid <uw@exom.dev>
2022-04-14 12:40:57 +0300

    add: item messages

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -27,6 +27,7 @@ public class Item
         new Item("Medkit", "Heals 10hp", "Sprites/UI/ItemMedkit", (player) =>
         {
             player.ModifyHP(10);
+            GameMaster.INSTANCE.guard.DisplayMessage(string.Format("{0} healed 10 health", player.name));
         }),
         new Item("Sniper", "Deals 10 damage to a random player", "Sprites/UI/ItemSniper", (player) =>
         {
@@ -38,6 +39,7 @@ public class Item
             } while(other.id == player.id);
 
             other.ModifyHP(-10);
+            GameMaster.INSTANCE.guard.DisplayMessage(string.Format("{0} sniped {1} for 10 health", player.name, other.name));
         }),
         new Item("Teleporter", "Swaps your place with someone else's", "Sprites/UI/ItemTeleporter", (player) =>
         {
@@ -52,6 +54,7 @@ public class Item
 
             other.TeleportToSpot(GameMaster.INSTANCE.guard.boardSpots[player.spot]);
             player.TeleportToSpot(GameMaster.INSTANCE.guard.boardSpots[otherSpot]);
+            GameMaster.INSTANCE.guard.DisplayMessage(string.Format("{0} got teleported", player.name));
         })
     };
 }
