a8397b3e09d23716c9f6480627e45f94dba87766
vladvlad00 <teodorescu.vlad@yahoo.com>
2022-06-01 13:05:36 +0300

    lobby

--- Assets/Scripts/Lobby/Play.cs
+++ Assets/Scripts/Lobby/Play.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
+using TMPro;
 
 public class Play : MonoBehaviour, IPointerClickHandler
 {
     public ColorPicker playerColor1;
     public ColorPicker playerColor2;
+    public TMP_InputField playerName1;
+    public TMP_InputField playerName2;
 
     public void OnPointerClick(PointerEventData eventData)
     {
         GameMaster.INSTANCE.playerData = new List<PlayerData>();
-        GameMaster.INSTANCE.playerData.Add(new PlayerData("Player1", GameMaster.playerColors[playerColor1.colorIndex]));
-        GameMaster.INSTANCE.playerData.Add(new PlayerData("Player2", GameMaster.playerColors[playerColor2.colorIndex]));
+        GameMaster.INSTANCE.playerData.Add(new PlayerData(playerName1.text, GameMaster.playerColors[playerColor1.colorIndex]));
+        GameMaster.INSTANCE.playerData.Add(new PlayerData(playerName2.text, GameMaster.playerColors[playerColor2.colorIndex]));
         TaleExtra.DisableInput();
         TaleExtra.RipOut();
         Tale.Scene("Scenes/Stranded");
